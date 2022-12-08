5413644f51ec0cc1043e04ce15760e958898bd63
Firstname Lastname <your_email@youremail.com>
2022-12-08 17:21:54 +0100

    find

---
+++ dist/config.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Parsing the env file.
+dotenv_1.default.config({ path: path_1.default.resolve(__dirname, '../.env') });
+// Loading process.env as ENV interface
+const getConfig = () => {
+    return {
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
+        MONGO_URI: process.env.MONGO_URI,
+        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+        JWT_SECRET: process.env.JWT_SECRET,
+    };
+};
+// Throwing an Error if any field was undefined we don't
+// want our app to run if it can't connect to DB and ensure
+// that these fields are accessible. If all is good return
+// it as Config which just removes the undefined from our type
+// definition.
+const getSanitzedConfig = (config) => {
+    for (const [key, value] of Object.entries(config)) {
+        if (value === undefined) {
+            throw new Error(`Missing key ${key} in config.env`);
+        }
+    }
+    return config;
+};
+const config = getConfig();
+const sanitizedConfig = getSanitzedConfig(config);
+exports.default = sanitizedConfig;
